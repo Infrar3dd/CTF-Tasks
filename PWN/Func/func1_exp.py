e5623ae228a03190fe692df0d6e1b22ce5b8b76a
Infrar3dd <120894421+Infrar3dd@users.noreply.github.com>
2025-09-22 17:12:36 +0300

    Update func1_exp.py

--- PWN/Func/func1_exp.py
+++ PWN/Func/func1_exp.py
@@ -3,9 +3,6 @@ import re
 
 context.arch = 'i386'
 
-HOST = "178.216.122.15"
-PORT = 43464
-
 elf = ELF('./func')
 log.info(f"puts@got: {hex(elf.got['puts'])}")
 log.info(f"give_flag: {hex(elf.symbols['give_flag'])}")
